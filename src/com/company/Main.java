ad5ff141ebbdc7d1c35c25e2424b0df0146a623f
unknown <knts2nttn@gmail.com>
2022-03-24 10:21:12 +0700

    baitap

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Fan fan1 =new Fan(3,true,"green",10);
+        Fan fan2 =new Fan(2,false,"black",5);
+        System.out.println("Trang thai cua quat 1 la: "+"\n"+fan1);
+        System.out.println("Trang thai cua quat 2 la: "+"\n"+fan2);
+    }
+    }
